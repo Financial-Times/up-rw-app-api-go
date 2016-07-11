96cec7a76a1e476f40063cf1438c39082800612b
Martin Garton <martin.garton@ft.com>
2016-07-11 14:41:12 +0100

    Initial commit of existing Service API definition

---
+++ rwapi/api.go
@@ -0,0 +1,16 @@
+package rwapi
+
+import (
+	"encoding/json"
+)
+
+// Service defines the functions any read-write application needs to implement
+type Service interface {
+	Write(thing interface{}) error
+	Read(uuid string) (thing interface{}, found bool, err error)
+	Delete(uuid string) (found bool, err error)
+	DecodeJSON(*json.Decoder) (thing interface{}, identity string, err error)
+	Count() (int, error)
+	Check() error
+	Initialise() error
+}
